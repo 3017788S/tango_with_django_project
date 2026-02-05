5f43117185a3b448e6f00a329bffd7da10e9ae37
Volodymyr Shkolnyi <3017788S@student.gla.ac.uk>
2026-02-05 15:52:34 +0000

    test_6_OK

--- rango/views.py
+++ rango/views.py
@@ -5,12 +5,14 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {}
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -35,7 +37,7 @@ def show_category(request, category_name_slug):
 
         context_dict['pages'] = pages
         context_dict['category'] = category
-        
+
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
