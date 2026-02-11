ba2981b7392a564f015be283592f9e26bd67c102
Volodymyr Shkolnyi <3017788S@student.gla.ac.uk>
2026-02-11 15:31:47 +0000

    chapter_9.6

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
@@ -18,6 +19,7 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
 
 class Page(models.Model):
     TITLE_MAX_LENGTH = 128
@@ -30,3 +32,12 @@ class Page(models.Model):
     
     def __str__(self):    
         return self.title
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
